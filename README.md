f682da28249ff3b3b6a086821048cd491d861c5a
Hugo <h@hugo.sx>
2017-03-02 21:54:09 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Micro Emoji 🎉
+A microservice for serving, listing and searching for emoji.
+
+Emoji icons courtesy of [EmojiOne](http://emojione.com), a free and open source project.
+
+# Usage
+```
+GET /:emojiName
+GET /:emojiName/64
+GET /:emojiName/128
+GET /:emojiName/512
+
+GET /list
+
+GET /search/:emojiName [not implemented]
+```
+
+[comment]: <> (TODO: Add Example GIF)
+
+# Contributing
+Clone the repo, `yarn install`, `yarn run dev`, change what you'd like and make a pull request ✌️
+
+# Deploy
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/hugomd/micro-emoji)
+
+```
+now hugomd/micro-emoji
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+const Hapi = require('hapi')
+const Inert = require('inert');
+const Emoji = require('emojione')
+const EmojiShortnames = require('./emoji-shortnames')
+const fs = require('fs-promise')
+
+const app = new Hapi.Server()
+
+app.connection({ port: 3000 })
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/list',
+    config: {
+      handler: async (req, res) => {
+        res(EmojiShortnames)
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/{shortname}/{size?}',
+    config: {
+      handler: async (req, res) => {
+        const shortname = req.params.shortname
+        const size = req.params.size
+
+        if (size && !['64', '128', '512'].includes(size)) {
+          return res('Size must be one of 64, 128, or 512.').code(400)
+        }
+        const image = Emoji.shortnameToImage(`:${shortname}:`)
+        const re = /png\/(.*).png/
+        const found = image.match(re)[1]
+
+        let filePath
+        switch (size) {
+          case '64':
+            filePath = `./node_modules/emojione/assets/png/${found}.png`
+            break;
+          case '128':
+            filePath = `./node_modules/emojione/assets/png_128x128/${found}.png`
+            break;
+          case '512':
+            filePath = `./node_modules/emojione/assets/png_512x512/${found}.png`
+            break;
+          default:
+            filePath = `./node_modules/emojione/assets/png/${found}.png`
+        }
+
+        try {
+          return res.file(filePath)
+        } catch (err) {
+          return res(err).code(400)
+        }
+      }
+    }
+  }
+]
+
+app.register([ Inert ], (err) => {
+  if (err) console.log(err)
+  app.route(routes)
+})
+
+app.on('response', (request) => {
+  console.log('Payload: ' + JSON.stringify(request.payload))
+  console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.url.path + ' --> ' + request.response.statusCode)
+})
+
+app.start((err) => {
+  if (err) throw err
+})
+
+module.exports = app
